7bbf5c8c132d4a1413628061c1f3328993806a21
kmww <kmw9708@gmail.com>
2023-01-09 14:59:57 +0900

    design: 404페이지 기본 스타일

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,5 +1,26 @@
+import styled from '@emotion/styled';
+
 const NotFound = () => {
-  return <div>404 Page Not Found!</div>;
+  return (
+    <WarningContainer>
+      <WarningStatus>404</WarningStatus>
+      <WarningText>Page Not Found!</WarningText>
+    </WarningContainer>
+  );
 };
 
 export default NotFound;
+
+const WarningContainer = styled.div`
+  text-align: center;
+  padding: 100px 20px;
+`;
+
+const WarningStatus = styled.div`
+  font-size: 160px;
+  color: royalblue;
+`;
+
+const WarningText = styled.div`
+  font-size: 50px;
+`;
