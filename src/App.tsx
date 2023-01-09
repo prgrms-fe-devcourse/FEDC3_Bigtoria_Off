4146acbc9c450a3a2bd613be510c807f94c972a4
kmww <kmw9708@gmail.com>
2023-01-10 01:00:18 +0900

    feat: 회원가입 버튼

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Header from './components/shared/Header';
+import SignUpButton from './components/signup/SignUpButton';
 
 import { ROUTES } from './constants/routes';
 
@@ -8,6 +9,7 @@ const App = () => {
   return (
     <BrowserRouter>
       <Header />
+      <SignUpButton />
       <Routes>
         <Route path={ROUTES.HOME} element={<div>Home</div>} />
       </Routes>
---
+++ src/components/signup/SignUpButton.tsx
@@ -0,0 +1,48 @@
+import { useState } from 'react';
+import { lightBlue } from '@mui/material/colors';
+import { Box, Button, CircularProgress } from '@mui/material';
+
+export default function CircularIntegration() {
+  const [loading, setLoading] = useState(false);
+
+  const handleButtonClick = () => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  };
+
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <Box
+        sx={{
+          width: '100%',
+          alignItems: 'center',
+          position: 'relative',
+        }}
+      >
+        <Button
+          fullWidth={true}
+          variant="contained"
+          disabled={loading}
+          onClick={handleButtonClick}
+        >
+          회원가입
+        </Button>
+        {loading && (
+          <CircularProgress
+            size={24}
+            sx={{
+              color: lightBlue[500],
+              position: 'absolute',
+              top: '50%',
+              left: '50%',
+              marginTop: '-12px',
+              marginLeft: '-12px',
+            }}
+          />
+        )}
+      </Box>
+    </Box>
+  );
+}
