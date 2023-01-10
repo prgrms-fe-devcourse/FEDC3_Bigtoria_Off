57de861f8c498f15a20fd3ce2c6709b0d690a80a
Yuri Kim <glassk0724@gmail.com>
2023-01-10 16:03:00 +0900

    rename: Story에서 StoryEdit로 폴더 이름 변경

--- src/components/StoryEdit/TextInput.tsx
+++ src/components/StoryEdit/TextInput.tsx
@@ -1,33 +1,25 @@
 import TextField from '@mui/material/TextField';
 
 interface Props {
-  label?: string;
   placeholder?: string;
-  variant: 'outlined' | 'filled' | 'standard';
   multiline?: boolean;
   rows?: number;
   fullWidth?: boolean;
-  margin?: 'none' | 'dense' | 'normal';
 }
 
 const TextInput = ({
-  label,
   placeholder,
-  variant,
   fullWidth = true,
   multiline = false,
   rows = 10,
-  margin = 'normal',
 }: Props) => {
   return (
     <TextField
-      label={label}
+      variant='outlined'
       placeholder={placeholder}
-      variant={variant}
       fullWidth={fullWidth}
       multiline={multiline}
       rows={rows}
-      margin={margin}
     />
   );
 };
