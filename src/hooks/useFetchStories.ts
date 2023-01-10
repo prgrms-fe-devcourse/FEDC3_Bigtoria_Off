92e92139d3a81937910a79a442729ce5b009e427
metacode22 <alohajune22@gmail.com>
2023-01-10 19:06:56 +0900

    refactor: useParams를 통해 특정 사용자의 stories를 받아오도록 변경

--- src/hooks/useFetchStories.ts
+++ src/hooks/useFetchStories.ts
@@ -66,9 +66,7 @@ const useFetchStories = () => {
       setIsLoading(true);
       try {
         const { data: fetchedStories } = await axios({
-          url: `${
-            import.meta.env.VITE_API_URL
-          }/posts/author/63bcf0d4f596c65f9ee2f226`,
+          url: `${import.meta.env.VITE_API_URL}/posts/author/${userId}`,
           method: 'GET',
         });
         setStories(fetchedStories);
