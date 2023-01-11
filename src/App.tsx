ec62098c2a838771208a0098de0505e4b205dce4
metacode22 <alohajune22@gmail.com>
2023-01-11 18:29:28 +0900

    feature: Signin 페이지 라우트 작성, 기본 디자인 구현

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './components/shared/Header';
 import { ROUTES } from './constants/routes';
 import HomePage from './pages/Home';
+import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import StoryBook from './pages/StoryBook';
 
@@ -14,6 +15,7 @@ const App = () => {
         <Route path={ROUTES.HOME} element={<HomePage />} />
         <Route path={ROUTES.STORY_BOOK} element={<StoryBook />} />
         <Route path={ROUTES.SIGNUP} element={<SignUp />} />
+        <Route path={ROUTES.SIGNIN} element={<SignIn />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/SignIn.tsx
@@ -0,0 +1,7 @@
+import SignInForm from '../components/SignIn/SignInForm';
+
+const SignIn = () => {
+  return <SignInForm />;
+};
+
+export default SignIn;
