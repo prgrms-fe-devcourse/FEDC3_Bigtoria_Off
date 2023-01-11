2227b39a6402580a523b162605de4f9532b5cbdf
kmww <kmw9708@gmail.com>
2023-01-12 02:54:52 +0900

    rename: SignUpForm -> 회원가입 페이지로 변경

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,7 +1,68 @@
-import SignUpForm from '../components/SignUp/SignUpForm';
+import { Box, Container } from '@mui/material';
+
+import SignUpSelector from '../components/SignUp/./SignUpSelector';
+import SignUpInput from '../components/SignUp//SignUpInput';
+import SignUpButton from '../components/SignUp/SignUpButton';
+import useSignUpForm from '../hooks/useSignUpForm';
 
 const SignUp = () => {
-  return <SignUpForm />;
+  const { values, isLoading, errors, handleSubmit, handleChange } =
+    useSignUpForm();
+
+  return (
+    <Container>
+      <form onSubmit={handleSubmit}>
+        <SignUpInput
+          placeholder='닉네임'
+          type='text'
+          name='fullName'
+          value={values.fullName}
+          onChange={handleChange}
+          errorMsg={errors.fullName}
+        />
+        <SignUpInput
+          placeholder='이메일 주소'
+          type='text'
+          name='email'
+          value={values.email}
+          onChange={handleChange}
+          errorMsg={errors.email}
+        />
+        <SignUpInput
+          placeholder='비밀번호'
+          type='password'
+          name='password'
+          value={values.password}
+          onChange={handleChange}
+          errorMsg={errors.password}
+        />
+        <SignUpInput
+          placeholder='비밀번호 확인'
+          type='password'
+          name='passwordConfirm'
+          value={values.passwordConfirm}
+          onChange={handleChange}
+          errorMsg={errors.passwordConfirm}
+        />
+        <Box sx={{ display: 'flex', gap: 2 }}>
+          <SignUpSelector
+            onChange={handleChange}
+            name='birth'
+            errorMsg={errors.birth}
+          />
+          <SignUpInput
+            placeholder='직업'
+            type='text'
+            name='career'
+            value={values.career}
+            onChange={handleChange}
+            errorMsg={errors.career}
+          />
+        </Box>
+        <SignUpButton isLoading={isLoading} />
+      </form>
+    </Container>
+  );
 };
 
 export default SignUp;
