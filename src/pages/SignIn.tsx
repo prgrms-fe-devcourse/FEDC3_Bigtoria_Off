e1e433f35154ccf6682aac0fb8189bbdc4e86408
metacode22 <alohajune22@gmail.com>
2023-01-12 15:28:12 +0900

    refactor: SignInForm 컴포넌트 구현

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,18 +1,7 @@
-import styled from '@emotion/styled';
-import {
-  Box,
-  Button,
-  CircularProgress,
-  Container,
-  Divider,
-  Grid,
-  Link,
-  TextField,
-  Typography,
-} from '@mui/material';
+import { Container, Divider, Grid, Link } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 
-import { color } from '../constants/color';
+import SignInForm from '../components/SignIn/SignInForm';
 import { ROUTES } from '../constants/routes';
 import useCheckAuthToken from '../hooks/useCheckAuthToken';
 import useSignInForm from '../hooks/useSignInForm';
@@ -37,86 +26,14 @@ const SignIn = () => {
   return (
     <>
       <Container component='main' maxWidth='xs' sx={{ padding: '1rem' }}>
-        <Box
-          component='form'
+        <SignInForm
+          values={values}
+          errors={errors}
+          isLoading={isLoading}
           onSubmit={handleSubmit}
-          sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            gap: '1rem',
-          }}
-        >
-          <Typography component='h1' variant='h5'>
-            로그인
-          </Typography>
-          <TextField
-            error={errors.email !== ''}
-            label='이메일'
-            name='email'
-            value={values.email}
-            autoFocus
-            autoComplete='email'
-            onChange={handleChange}
-            fullWidth
-          />
-          {errors.email && <Text>{errors.email}</Text>}
-          <TextField
-            error={errors.password !== ''}
-            label='비밀번호'
-            name='password'
-            type='password'
-            value={values.password}
-            autoComplete='current-password'
-            onChange={handleChange}
-            fullWidth
-          />
-          {errors.password && <Text>{errors.password}</Text>}
-          <Grid container sx={{ gap: '1rem' }}>
-            <Grid
-              item
-              xs
-              sx={{
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'center',
-              }}
-            >
-              <Button
-                disabled={isLoading}
-                variant='contained'
-                type='submit'
-                fullWidth
-              >
-                로그인
-              </Button>
-              {isLoading && (
-                <CircularProgress size={24} sx={{ position: 'absolute' }} />
-              )}
-            </Grid>
-            <Grid
-              item
-              xs
-              sx={{
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'center',
-              }}
-            >
-              <Button
-                disabled={isLoading}
-                variant='outlined'
-                fullWidth
-                onClick={handleClickButtonWithoutSignIn}
-              >
-                로그인 없이 접속
-              </Button>
-              {isLoading && (
-                <CircularProgress size={24} sx={{ position: 'absolute' }} />
-              )}
-            </Grid>
-          </Grid>
-        </Box>
+          onChange={handleChange}
+          onClick={handleClickButtonWithoutSignIn}
+        />
         <Divider
           sx={{
             marginTop: '1rem',
@@ -141,8 +58,3 @@ const SignIn = () => {
 };
 
 export default SignIn;
-
-const Text = styled.div`
-  color: ${color.error};
-  align-self: start;
-`;
