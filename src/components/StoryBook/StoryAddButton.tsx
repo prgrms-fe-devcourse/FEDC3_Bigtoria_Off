a7828470b1958d20fc652dbd32578a6a874b8e15
metacode22 <alohajune22@gmail.com>
2023-01-13 22:18:00 +0900

    feature: StoryAddButton 클릭 시 스토리 추가 및 로그인 페이지로 리다이렉팅

--- src/components/StoryBook/StoryAddButton.tsx
+++ src/components/StoryBook/StoryAddButton.tsx
@@ -1,13 +1,20 @@
 import { Button } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 
+import { checkAuth } from '../../apis/auth';
+import { TOKEN_KEY } from '../../constants/auth';
 import { ROUTES } from '../../constants/routes';
+import { getLocalStorage } from '../../utils/storage';
 
 const StoryAddButton = () => {
   const navigate = useNavigate();
 
-  const handleClick = () => {
-    navigate(ROUTES.STORY_CREATE);
+  const handleClick = async () => {
+    const token = getLocalStorage(TOKEN_KEY);
+    const { isOnline } = await checkAuth();
+
+    token && isOnline && navigate(ROUTES.STORY_CREATE);
+    (!token || !isOnline) && navigate(ROUTES.SIGNIN);
   };
 
   return (
