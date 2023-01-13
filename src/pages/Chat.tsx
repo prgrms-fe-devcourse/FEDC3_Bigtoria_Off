4e4d6c499924e3b5258f28f3baf464136bbfeae5
tooooo1 <wer4272@nate.com>
2023-01-13 21:55:26 +0900

    style: 채팅 기본 레이아웃 구성

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -1,5 +1,15 @@
+import styled from '@emotion/styled';
+import {
+  Avatar,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemButton,
+  ListItemText,
+  Typography,
+} from '@mui/material';
 import axios from 'axios';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import http from '../apis/instance';
 import { TOKEN_KEY } from '../constants/auth';
@@ -24,6 +34,7 @@ interface Message {
 }
 
 const Chat = () => {
+  const messageInputRef = useRef<HTMLTextAreaElement>(null);
   const [token, setToken] = useState(getLocalStorage(TOKEN_KEY) ?? '');
   const [message, setMessage] = useState<Message[]>([]);
   const [specificUser, setSpecificUser] = useState<Message[]>([]);
@@ -90,6 +101,14 @@ const Chat = () => {
     specificUser.map((messageTotal) => messageTotal);
   }, [specificUser, message]);
 
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+  };
+
+  const handleOnKeyPress = (event: React.KeyboardEvent) => {
+    if (event.key === 'Enter' && event.shiftKey) event.preventDefault();
+  };
+
   return (
     <div>
       <button onClick={getUserList}>유저 목록 불러오기</button>
@@ -110,17 +129,107 @@ const Chat = () => {
       <button onClick={roadMessages}>
         특정사용자와의 메세지 목록 불러오기
       </button>
-      {specificUser?.map((messageTotal) => {
-        return (
-          <div key={messageTotal.createdAt}>
-            <div>
-              {messageTotal.message} {messageTotal.seen ? '봤음' : '안 봤음'}
-            </div>
-          </div>
-        );
-      })}
+
+      <Wrap>
+        <List sx={{ width: '35%' }}>
+          {[0, 1, 2, 3].map((value) => {
+            return (
+              <ListItem key={value} disablePadding>
+                <ListItemButton>
+                  <ListItemAvatar>
+                    <Avatar alt={`Avatar n°${value + 1}`} src={''} />
+                  </ListItemAvatar>
+                  <ListItemText
+                    id={'1'}
+                    primary={`퉁이리`}
+                    secondary={
+                      <Typography
+                        component='span'
+                        variant='body2'
+                        color='text.primary'>
+                        <TypoContents>
+                          안녕하세요<div>22:20</div>
+                        </TypoContents>
+                      </Typography>
+                    }
+                  />
+                </ListItemButton>
+              </ListItem>
+            );
+          })}
+        </List>
+        <ChatWrapper>
+          {specificUser?.map((messageTotal) => {
+            return (
+              <div key={messageTotal.createdAt}>
+                <div>
+                  {messageTotal.message}{' '}
+                  {messageTotal.seen ? '봤음' : '안 봤음'}
+                </div>
+              </div>
+            );
+          })}
+          <MessageInputFormWrap onSubmit={handleSubmit}>
+            <MessageInput
+              ref={messageInputRef}
+              data-testid='textarea'
+              cols={30}
+              rows={5}
+              onKeyPress={handleOnKeyPress}
+            />
+            <MessageInputSubmitButton data-testid='submit' type='submit'>
+              보내기
+            </MessageInputSubmitButton>
+          </MessageInputFormWrap>
+        </ChatWrapper>
+      </Wrap>
     </div>
   );
 };
 
 export default Chat;
+
+const Wrap = styled.div`
+  display: flex;
+  min-height: 50vh;
+`;
+
+const TypoContents = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const ChatWrapper = styled.div`
+  width: 100%;
+`;
+
+const MessageInputFormWrap = styled.form`
+  position: relative;
+  bottom: 0px;
+  width: 100%;
+  background-color: white;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const MessageInput = styled.textarea`
+  all: unset;
+  width: 70%;
+  height: 55px;
+  border: 1px solid #e2e5e6;
+  border-radius: 8px;
+  padding: 4px;
+`;
+
+const MessageInputSubmitButton = styled.button`
+  all: unset;
+  width: 20%;
+  height: 55px;
+  border: 1px solid #e2e5e6;
+  border-radius: 8px;
+  text-align: center;
+  cursor: pointer;
+  padding: 4px;
+`;
