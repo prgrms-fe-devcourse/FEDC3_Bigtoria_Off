bb495e0f1f5780214181a1cc7e8c064c17303ce6
Kal-MH <rkfalgus713@gmail.com>
2023-01-16 00:53:25 +0900

    refactor: alarm logic 변경

--- src/components/Alarm/NotificationButton.tsx
+++ src/components/Alarm/NotificationButton.tsx
@@ -2,7 +2,6 @@ import { Notifications } from '@mui/icons-material';
 import { Box } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 
-import { checkAuth } from '../../apis/auth';
 import { TOKEN_KEY } from '../../constants/auth';
 import { ROUTES } from '../../constants/routes';
 import { getLocalStorage } from '../../utils/storage';
@@ -14,13 +13,8 @@ const AlarmButton = () => {
 
   const handleClick = async () => {
     const token = getLocalStorage(TOKEN_KEY);
-    const { isOnline } = await checkAuth();
 
-    if (token && isOnline) {
-      navigate(NOTIFICATION);
-    } else if (!token || !isOnline) {
-      navigate(SIGNIN);
-    }
+    token ? navigate(NOTIFICATION) : navigate(SIGNIN);
   };
 
   return (
