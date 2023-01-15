aa4db3d1711de378c38ad35db0ec82756c375417
Yuri Kim <glassk0724@gmail.com>
2023-01-16 00:31:14 +0900

    feat: window.scrollTo behavior 추가

--- src/components/shared/ScrollToTop.tsx
+++ src/components/shared/ScrollToTop.tsx
@@ -5,7 +5,7 @@ const ScrollToTop = () => {
   const { pathname } = useLocation();
 
   useEffect(() => {
-    window.scrollTo(0, 0);
+    window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
   }, [pathname]);
 
   return null;
