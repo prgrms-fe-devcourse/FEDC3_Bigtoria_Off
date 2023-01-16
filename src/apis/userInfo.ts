24054bae25c847ab17181ff8e381350c9be22669
metacode22 <alohajune22@gmail.com>
2023-01-16 15:23:23 +0900

    refactor: userInfo api의 url 상수화

--- src/apis/userInfo.ts
+++ src/apis/userInfo.ts
@@ -1,9 +1,10 @@
+import { API_URLS } from './../constants/apiUrls';
 import http from './instance';
 
 export const userInfo = async (userId: string) => {
   try {
     const { data } = await http.get({
-      url: `/users/${userId}`,
+      url: API_URLS.USER.GET_USER_INFO(userId),
     });
     return data;
   } catch (error) {
