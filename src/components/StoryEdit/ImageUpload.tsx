6759cc81e3e199e0602223d7440aae48a0cc5e92
Yuri Kim <glassk0724@gmail.com>
2023-01-17 03:27:03 +0900

    feat: 이미지 드래그앤드롭 업로드

---
+++ src/components/StoryEdit/ImageUpload.tsx
@@ -0,0 +1,114 @@
+import styled from '@emotion/styled';
+import { Box, Button } from '@mui/material';
+import { ChangeEvent, DragEvent, useRef, useState } from 'react';
+
+interface Props {
+  src: string;
+  onChange: (file: File) => void;
+  onDelete: () => void;
+}
+
+const ImageUpload = ({ src, onChange, onDelete }: Props) => {
+  const [currentImage, setCurrentImage] = useState<File>();
+  const [dragging, setDragging] = useState(false);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      setCurrentImage(file);
+      onChange(file);
+    }
+  };
+
+  const handleChooseFile = () => {
+    if (inputRef.current) {
+      inputRef.current.click();
+    }
+  };
+
+  const handleDragEnter = (e: DragEvent) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    if (e.dataTransfer.items?.length > 0) {
+      setDragging(true);
+    }
+  };
+  const handleDragLeave = (e: DragEvent) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    setDragging(false);
+  };
+  const handleDragOver = (e: DragEvent) => {
+    e.preventDefault();
+    e.stopPropagation();
+  };
+  const handleFileDrop = (e: DragEvent) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    const file = e.dataTransfer.files?.[0];
+    if (file) {
+      setCurrentImage(file);
+      onChange(file);
+    }
+  };
+
+  return (
+    <>
+      <UploadContainer
+        sx={{
+          borderColor: dragging ? 'black' : 'grey',
+        }}
+        onClick={handleChooseFile}
+        onDrop={handleFileDrop}
+        onDragEnter={handleDragEnter}
+        onDragLeave={handleDragLeave}
+        onDragOver={handleDragOver}>
+        <input
+          hidden
+          ref={inputRef}
+          type='file'
+          name='Story Image'
+          accept='image/jpg, image/jpeg, image/png'
+          onChange={handleFileChange}
+        />
+        {src ? (
+          <ImagePreview src={src} alt='story image' />
+        ) : (
+          <ImagePlaceholder>
+            클릭하여 이미지를 추가하거나
+            <br />
+            이미지를 드래그하세요
+          </ImagePlaceholder>
+        )}
+      </UploadContainer>
+      {(src || currentImage) && <Button onClick={onDelete}>삭제</Button>}
+    </>
+  );
+};
+
+export default ImageUpload;
+
+const UploadContainer = styled(Box)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 300px;
+  border: 1px dashed grey;
+`;
+
+const ImagePlaceholder = styled.div`
+  color: grey;
+  text-align: center;
+  line-height: 1.4rem;
+`;
+
+const ImagePreview = styled.img`
+  max-width: 100%;
+  height: 300px;
+  object-fit: contain;
+`;
