97ab5c4e4ec9279b858fb15c85358635940b522f
metacode22 <alohajune22@gmail.com>
2023-01-17 17:49:09 +0900

    fix: useCheckAuthToken에서 isOnline 제거 및 여러 번 뒤로가기 클릭 시 발생하는 에러 해결

--- src/hooks/useCheckAuthToken.ts
+++ src/hooks/useCheckAuthToken.ts
@@ -4,21 +4,14 @@ import { useNavigate } from 'react-router-dom';
 import { TOKEN_KEY } from '../constants/auth';
 import { ROUTES } from '../constants/routes';
 import { getLocalStorage } from '../utils/storage';
-import { checkAuth } from './../apis/auth';
 
 const useCheckAuthToken = () => {
   const navigate = useNavigate();
   const token = getLocalStorage(TOKEN_KEY);
 
   useEffect(() => {
-    const fetchIsOnline = async () => {
-      const { isOnline } = await checkAuth();
-
-      token && isOnline && navigate(ROUTES.HOME);
-    };
-
-    fetchIsOnline();
-  }, []);
+    token && navigate(ROUTES.HOME);
+  });
 };
 
 export default useCheckAuthToken;
