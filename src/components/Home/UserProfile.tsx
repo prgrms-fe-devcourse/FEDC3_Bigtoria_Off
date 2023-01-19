f4e9d17d87310f2f633fac5212c4cdfaad7dde50
Kal-MH <rkfalgus713@gmail.com>
2023-01-20 03:37:43 +0900

    design: NotificationMsg, UserProfile borderRadius, Padding 적용 및 사진 크기 조절

--- src/components/Home/UserProfile.tsx
+++ src/components/Home/UserProfile.tsx
@@ -34,11 +34,11 @@ const UserProfile = ({ path, image, fullName, job, year }: Props) => {
       <ListItemButton
         onClick={() => navigate(path)}
         sx={{ padding: '18px 20px', borderRadius: 3.5 }}>
-        <ListItemAvatar sx={{ marginRight: '10px' }}>
+        <ListItemAvatar sx={{ marginRight: '15px' }}>
           <Avatar
             sx={{
-              width: 42,
-              height: 42,
+              width: 54,
+              height: 54,
               backgroundColor: `${!image ? COLORS.SUB : ''}`,
             }}
             alt={fullName}
@@ -50,7 +50,12 @@ const UserProfile = ({ path, image, fullName, job, year }: Props) => {
           secondary={
             <React.Fragment>
               <Typography sx={{ display: 'inline' }} component='span'>
-                {year} {job}
+                {year}
+              </Typography>
+              <Typography
+                sx={{ display: 'inline', marginLeft: '7px' }}
+                component='span'>
+                {job}
               </Typography>
             </React.Fragment>
           }></ListItemText>
