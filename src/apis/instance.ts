406ed91e1f7e04cc96016acf65c019d75fc59b6f
퉁이리 <wer4272@nate.com>
2023-01-20 00:21:56 +0900

    refactor: develop MODE

--- src/apis/instance.ts
+++ src/apis/instance.ts
@@ -4,8 +4,13 @@ import { TOKEN_KEY } from '../constants/auth';
 import { HTTP_METHODS } from '../constants/http';
 import { getLocalStorage } from '../utils/storage';
 
+const API_BASE_URL =
+  import.meta.env.MODE === 'development'
+    ? import.meta.env.VITE_API_URL
+    : '/api';
+
 const axiosInstance: AxiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: API_BASE_URL,
 });
 
 const handleRequest = (config: AxiosRequestConfig) => {
